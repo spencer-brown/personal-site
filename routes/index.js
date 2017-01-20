49d40d66c45c1d7bf072c1d0dda998cdc40e3dac
Spencer Brown <me@spencer.sx>
2017-01-20 16:18:54 +0700

    Add first writing

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ function router(app) {
   app.get('/',  (req, res, next) => {
     res.render('index');
   });
+
+  // TODO: Use some actual blogging software.
+  app.get('/htwfip', (req, res, next) => {
+    res.render('htwfip');
+  });
 }
 
 module.exports = router;
