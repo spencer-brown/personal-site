11c37d4849a10e687d3c365f1fc8fe076a277a0e
Spencer Brown <me@spencer.sx>
2017-04-26 21:20:20 -0700

    Add GA to build.js

--- public/js/build.js
+++ public/js/build.js
@@ -5,3 +5,6 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
 ;$(() => {
   console.log('🔥🔥🔥');
 });
+;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); 
+ga('create', 'UA-45372902-2', 'auto');
+ga('send', 'pageview');
